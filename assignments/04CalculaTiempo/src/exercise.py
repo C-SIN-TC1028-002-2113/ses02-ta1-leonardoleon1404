4050e9e0dd2a2093bd094fdb850e002687a5fac3
leonardoleon1404 <A01740974@itesm.mx>
2021-08-17 04:12:56 +0000

    Tarea 1 CalculaTiempo y Lustros

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    edad1 = int(input("Dame tu edad: "))
+    años1 = int(input("Dame el año actual: "))
+    años2 = años1+(100-edad1)
+    print("Cumplirás 100 años en el año:",años2)
+    
 
 if __name__ == '__main__':
     main()
--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    año1 = float(input("Dame el año de nacimiento: "))
+    año2 = float(input("Dame el año actual: "))
+    lustros = (año2-año1)/5
+    print("Los lustros que has vivido son:",lustros)
 
 
 
